c71bd95c0e8b0111e2053e09a795ed180dfdacd2
Fithri <mfithri.sjamsuddin@gmail.com>
2026-08-04 13:13:32 +0800

    Add files via upload

---
+++ amazon-lex_chatbot/Part 3: Connect Your Lex Chatbot to Lambda/fun_twist.py
@@ -0,0 +1,4 @@
+# Add a fun twist to the message
+    descriptions = ["YOUR-NAME's top-secret digital vault", "your personal treasure box", "bank of YOUR-NAME"]
+    description = random.choice(descriptions)
+    text = f"Hello! Your {account} account balance, tucked away in {description}, is currently ${balance}."
